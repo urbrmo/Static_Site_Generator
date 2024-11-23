4b51ac4b69ded3f5bb49ee8f24537c344f3e977e
urielbrunomota <ubumo@hotmail.com>
2024-11-23 12:33:18 -0500

    B: added .gitignore, main.sh, and src directory

---
+++ src/textnode.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class TextType(Enum):
+    Normal_text = "Normal"
+    Bold_text = "Bold"
+    Itallic_text = "Itallics"
+    Code_text = "Code"
+    Links = "link"
+    Images = "Image"
