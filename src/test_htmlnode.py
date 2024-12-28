890cd4ba6f6780e2b586863300b01bad0a2767cf
urielbrunomota <ubumo@hotmail.com>
2024-12-28 14:07:38 -0500

    added extra tests to test_htmlnode.py

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -10,5 +10,21 @@ class Testhtmlnode(unittest.TestCase):
         self.assertEqual(node.children, [])
         self.assertEqual(node.props, {"href": "https://example.com", "target": "_blank"})
 
+    def test_default_initialization(self):
+        node = HTMLNode()
+        self.assertIsNone(node.tag)
+        self.assertIsNone(node.value)
+        self.assertEqual(node.children, [])
+        self.assertEqual(node.props, {})
+
+    def test_props_to_html(self):
+        node = HTMLNode(props={"href": "https://example.com", "target": "_blank"})
+        self.assertEqual(node.props_to_html(), ' href="https://example.com" target="_blank"')
+
+    def test_repr_method(self):
+        node = HTMLNode(tag="div", value="Content", children=[HTMLNode(tag="span")], props={"class": "container"})
+        expected_repr = "HTMLNode(div, Content, [HTMLNode(span, None, [], {})], {'class': 'container'})"
+        self.assertEqual(repr(node), expected_repr)
+
 if __name__ == '__main__':
     unittest.main()
